67d4a1472baaf3ffd48d4e4155511875e477fc6c
vlad <vlad.kharkovskiy10@gmail.com>
2025-05-19 13:52:02 +0300

    fix structure

--- services/weather-schedule-service.js
+++ services/weather-schedule-service.js
@@ -9,46 +9,45 @@ dayjs.extend(utc);
 
 class WeatherScheduleService {
     start() {
-    
-    cron.schedule('*/5 * * * *', async () => {
-    	const subscriptions = await Subscription.findAll({
-            where: {
-                email_confirmed: true,
-            },
-        });
-		const now = dayjs.utc();
-        
-		for (const subscription of subscriptions) {
-            const lastSent = dayjs.utc(subscription.last_sent_at);
-
-            const diffMinutes = now.diff(lastSent, 'minute');
-
-			const shouldSend =
-			(subscription.frequency === 'hourly' && diffMinutes >= 60) ||
-			(subscription.frequency === 'daily' && diffMinutes >= 1140);
-
-			if (shouldSend) {
-				try {
-					const weatherServiceResponse = await weatherService.parseWeatherByCity(subscription.city);
-					const unsubscribeLink = `${config.server.BASE_URL}/unsubscribe/${subscription.unsubscribe_token}`;
-
-					await mailService.sendWeather(subscription.email, 
-									weatherServiceResponse.data.current.temp_c, 
-									weatherServiceResponse.data.current.humidity, 
-									weatherServiceResponse.data.current.condition.text, 
-									unsubscribeLink);
+        cron.schedule('*/5 * * * *', async () => {
+            const subscriptions = await Subscription.findAll({
+                where: {
+                    email_confirmed: true,
+                },
+            });
+            const now = dayjs.utc();
             
-					subscription.last_sent_at = dayjs.utc().toDate();
-					await subscription.save();
-				} catch (err) {
-					console.error(`Error sending weather to ${subscription.email}:`, err);
-				}
-			}
-		}
-		});
+            for (const subscription of subscriptions) {
+                const lastSent = dayjs.utc(subscription.last_sent_at);
 
-		console.log('WeatherScheduler started: running every 5 minutes');
-	}
+                const diffMinutes = now.diff(lastSent, 'minute');
+
+                const shouldSend =
+                (subscription.frequency === 'hourly' && diffMinutes >= 60) ||
+                (subscription.frequency === 'daily' && diffMinutes >= 1140);
+
+                if (shouldSend) {
+                    try {
+                        const weatherServiceResponse = await weatherService.parseWeatherByCity(subscription.city);
+                        const unsubscribeLink = `${config.server.BASE_URL}/unsubscribe/${subscription.unsubscribe_token}`;
+
+                        await mailService.sendWeather(subscription.email, 
+                            weatherServiceResponse.data.current.temp_c, 
+                            weatherServiceResponse.data.current.humidity, 
+                            weatherServiceResponse.data.current.condition.text, 
+                            unsubscribeLink);
+                
+                        subscription.last_sent_at = dayjs.utc().toDate();
+                        await subscription.save();
+                    } catch (err) {
+                        console.error(`Error sending weather to ${subscription.email}:`, err);
+                    }
+                }  
+            }
+        });
+
+        console.log('WeatherScheduler started: running every 5 minutes');
+    }
 }
 
 module.exports = new WeatherScheduleService();
