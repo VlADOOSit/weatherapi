628d2f3346b96690ec257fff29f2a8ec680187f9
vlad <vlad.kharkovskiy10@gmail.com>
2025-05-19 10:31:04 +0300

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const ErrorHandler = require('./Middlewares/errorHandler');
 
@@ -8,6 +9,7 @@ const SubscriptionRouter = require('./Routes/subscription-router');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/weatherapi.app/api', SubscriptionRouter);
